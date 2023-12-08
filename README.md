bf0623f7d39993590b4184211892e2ad780cd8e2
sami.rolina <sami.rolina@outlook.fr>
2023-12-08 14:06:41 +0100

    redaction du README

---
+++ README.md
@@ -0,0 +1,13 @@
+# Bienvenue dans le suivit de l'application web MacroMaster
+
+ *Maîtrisez votre diète, dominez votre forme.*
+
+## Objectifs:
+
+* Gérer votre régimes alimentaire via le décompte des macros alimentaire.
+* Créer vos recettes personnalisées.
+* Ajouter vos aliments préférés.
+
+## Documentation:
+
+Aller voir la documentation du projet dans le dossier mkdoc.
--- app/templates/home/home.tpl.php
+++ app/templates/home/home.tpl.php
@@ -13,7 +13,7 @@
         <div class="sidebar pe-4 pb-3">
             <nav class="navbar bg-secondary navbar-dark">
                 <a href="index.html" class="navbar-brand mx-4 mb-3">
-                    <h3 class="text-primary"><i class="fa fa-user-edit me-2"></i>DarkPan</h3>
+                    <h3 class="text-primary"><i class="fa fa-user-edit me-2"></i>MacroMaster</h3>
                 </a>
                 <div class="d-flex align-items-center ms-4 mb-4">
                     <div class="position-relative">
@@ -21,7 +21,7 @@
                         <div class="bg-success rounded-circle border border-2 border-white position-absolute end-0 bottom-0 p-1"></div>
                     </div>
                     <div class="ms-3">
-                        <h6 class="mb-0">Jhon Doe</h6>
+                        <h6 class="mb-0">Sami Rolina</h6>
                         <span>Admin</span>
                     </div>
                 </div>
